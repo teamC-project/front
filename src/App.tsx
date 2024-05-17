f371fabcb5ed3661266892a562fe334ef8ffaf48
ahndb <ahndb.dev@gmail.com>
2024-05-17 17:44:55 +0900

    Updating layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import "./App.css";
+import { useCookies } from "react-cookie";
+import { Route, Routes, useNavigate } from "react-router";
+import { MAIN_OFF_PATH, MAIN_ON_PATH } from "./constant";
+
+// component: root 경로 컴포넌트 //
+function Index() {
+
+  //                    state                    //
+  const [cookies] = useCookies();
+
+  //                    function                    //
+  const navigator = useNavigate();
+
+  //                    effect                    //
+  useEffect(() => {
+    const accessToken = cookies.accessToken;
+    if (accessToken) navigator(MAIN_ON_PATH);
+    else navigator(MAIN_OFF_PATH);
+  }, []);
+
+  //                    render                    //
+  return <></>;
+}
 
 function App() {
-  return <></>;
+  return (
+    <Routes >
+      <Route index element={<Index />} />
+    </Routes>
+  );
 }
 
 export default App;
