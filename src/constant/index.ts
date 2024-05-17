8028e07918f80a9302a834117f9f3aa0c4d28d01
GODDONGHYUN <leenl1168@gmail.com>
2024-05-17 12:31:36 +0900

    22

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -4,7 +4,6 @@ export const TREND_BOARD_PATH = "trend_board"
 export const TREND_BOARD_WRITE_PATH  = "write";
 export const TREND_BOARD_DETAIL_PATH = ":trendBoardNumber";
 export const TREND_BOARD_UPDATE_PATH = "update/:trendBoardNumber";
-export const TREND_BOARD_DELETE_PATH = "delete/:trendBoardNumber";
 
 // description: Navigation 절대 URL PATH
 // export const AUTH_ABSOLUTE_PATH = AUTH_PATH;
@@ -12,7 +11,7 @@ export const TREND_BOARD_DELETE_PATH = "delete/:trendBoardNumber";
 // description: API URL PATH
 export const SERVER_DOMAIN_URL = "http://localhost:4200";
 export const SERVER_API_URL = `${SERVER_DOMAIN_URL}/api/v1`;
-
+export const SERVICE_URL = '/service';
 export const SERVER_AUTH_MODULE_URL = `${SERVER_API_URL}/auth`;
 export const SIGN_IN_REQUEST_URL = `${SERVER_AUTH_MODULE_URL}/sign_in`;
 export const EMAIL_AUTH_REQUEST_URL = `${SERVER_AUTH_MODULE_URL}/email_auth`;
@@ -25,17 +24,29 @@ export const USER_DELETE_URL = `${SERVER_AUTH_MODULE_URL}/user_delete`;
 export const SERVER_TREND_BOARD_MODULE_URL = `${SERVER_API_URL}/trend_board`;
 
 // description : 트렌드 절대 URL PATH
-export const TREND_BOARD_ABSOLUTE_PATH = `${SERVER_API_URL}/ ${SERVER_TREND_BOARD_MODULE_URL}`;
-export const TREND_BOARD_LIST_ABSOLUTE_PATH = `${SERVER_API_URL}/${TREND_BOARD_PATH}`
-export const TREND_BOARD_WRITE_ABSOLUTE_PATH = `${SERVER_API_URL}/${TREND_BOARD_PATH}/${TREND_BOARD_WRITE_PATH}`;
+export const TREND_BOARD_ABSOLUTE_PATH = `${SERVICE_URL}/ ${SERVER_TREND_BOARD_MODULE_URL}`;
+export const TREND_BOARD_LIST_ABSOLUTE_PATH = `${SERVICE_URL}/${TREND_BOARD_PATH}`
+export const TREND_BOARD_WRITE_ABSOLUTE_PATH = `${SERVICE_URL}/${TREND_BOARD_PATH}/${TREND_BOARD_WRITE_PATH}`;
 export const TREND_BOARD_DETAIL_ABSOLUTE_PATH =(trendBoardNumber : number | string) => 
-	`${SERVER_API_URL}/${TREND_BOARD_PATH}/${trendBoardNumber}`;
+	`${SERVICE_URL}/${TREND_BOARD_PATH}/${trendBoardNumber}`;
 export const TREND_BOARD_UPDATE_ABSOLUTE_PATH = (trendBoardNumber : number | string) =>
-	`${SERVER_API_URL}/${TREND_BOARD_PATH}/update${trendBoardNumber}`
+	`${SERVICE_URL}/${TREND_BOARD_PATH}/update/${trendBoardNumber}`
 
 
 // description: Navigation 절대 URL PATH
 // export const AUTH_ABSOLUTE_PATH = AUTH_PATH;
+
+
+// description : 트렌드 API URL PATH 
+export const TREND_BOARD_MODULE_URL = `${SERVER_API_URL}/trend_board`;
+export const POST_TREND_BOARD_WRITE_URL = `${SERVER_TREND_BOARD_MODULE_URL}/`;
+export const GET_TREND_BOARD_LIST_URL = `${SERVER_TREND_BOARD_MODULE_URL}/list`
+export const GET_TREND_BOARD_SEARCH_URL = `${SERVER_TREND_BOARD_MODULE_URL}/list/search`;
+export const GET_TREND_BOARD_DETAIL_URL = (trendBoardNumber : number | string) => `${SERVER_TREND_BOARD_MODULE_URL}/${trendBoardNumber}`;
+export const PATCH_TREND_BOARD_LIKE_COUNT_URL = (trendBoardNumber : number | string) => `${SERVER_TREND_BOARD_MODULE_URL}/${trendBoardNumber}/like_count`
+export const DELETE_TREND_BOARD_DELETE_URL = (trendBoardNumber : number | string) => `${SERVER_TREND_BOARD_MODULE_URL}/${trendBoardNumber}`;
+export const PUT_TREND_BOARD_PUT_URL = (trendBoardNumber : number | string) => `${SERVER_TREND_BOARD_MODULE_URL}/${trendBoardNumber}`;
+
 
 
 
@@ -49,7 +60,7 @@ export const GET_ANNONOUNCEMENT_BOARD_DETAIL_URL = (announcementBoardNumber: num
 export const PATCH_ANNOUNCEMENT_BOARD_INCREASE_VIEW_COUNT_URL =  (announcementBoardNumber: number| string) => `${SERVER_ANNOUNCEMENT_BOARD_MODULE_URL}/${announcementBoardNumber}/increase_announcement_view_count`;
 export const DELETE_ANNOUCEMENT_BOARD_DELETE_URL = 
 (announcementBoardNumber: number| string) => `${SERVER_ANNOUNCEMENT_BOARD_MODULE_URL}/${announcementBoardNumber}`;
-export const POST_ANNOUNCEMENT_BOARD_PUT_URL = (announcementBoardNumber: number| string) => `${SERVER_ANNOUNCEMENT_BOARD_MODULE_URL}/${announcementBoardNumber}`;
+export const PUT_ANNOUNCEMENT_BOARD_PUT_URL = (announcementBoardNumber: number| string) => `${SERVER_ANNOUNCEMENT_BOARD_MODULE_URL}/${announcementBoardNumber}`;
 
 // description : 디자이너 API URL PATH
 export const SERVER_DESIGNER_BOARD_MODULE_URL = `${SERVER_API_URL}/designer_board`;
