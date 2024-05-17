5c41be0196feb66a81206239d97700d797181049
ahndb <ahndb.dev@gmail.com>
2024-05-17 10:15:05 +0900

    240517

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,178 +1,22 @@
-Auth 
-
-<h1 style='background-color: rgba(55, 55, 55, 0.4); text-align: center'>API 명세서 </h1>
-
-해당 API 명세서는 '헤어케어 디자이너 매칭 플랫폼 서비스'의 REST API를 명세하고 있습니다.
-
-- Domain : <http://localhost:4200>  
-
-***
-
-<h2 style='background-color: rgba(55, 55, 55, 0.2); text-align: center'>Auth 모듈</h2>
-
-인증 및 인가와 관련된 REST API 모듈  
-로그인, 회원가입, 소셜 로그인, 소셜 회원가입 등의 API가 포함되어 있습니다.  
-
-- url : /api/v1/auth
+// description : Navigation URL PATH
+export const SNS_PATH = "/sns/:accessToken/:expires";
+export const AUTH_PATH = "/authentication";
 
 
-
-#### - 이메일 인증  
-
-- method : **POST**     
-- URL : **/email_auth**  
-
-```bash
-curl -v -X POST "http://localhost:4200/api/v1/auth/email-auth" \
- -d "userEmail=email@email.com"
-```
+// description: Navigation 절대 URL PATH
+export const AUTH_ABSOLUTE_PATH = AUTH_PATH;
 
 
+// description: API URL PATH
+export const SERVER_DOMAIN_URL = "http://localhost:4000";
+export const SERVER_API_URL = `${SERVER_DOMAIN_URL}/api/v1`;
+export const SERVER_AUTH_MODULE_URL = `${SERVER_API_URL}/auth`;
 
-#### - 이메일 인증 확인
-
-- method : **POST**  
-- URL : **/email_auth_check**  
-
-```bash
-curl -v -X POST "http://localhost:4200/api/v1/auth/email-auth-check" \
- -d "userEmail=email@email.com" \
- -d "authNumber=0123"
-```
-
-***
-
-#### - 회원가입
-
-- method : **POST**  
-- URL : **/sign_up**  
-
-```bash
-curl -v -X POST "http://localhost:4200/api/v1/auth/sign_up" \
- -d "userId=service123" \
- -d "userPassword=Pa55w0rd" \
- -d "userEmail=email@email.com" \
- -d "authNumber=0123"  \
- -d "userAge":"20" \
- -d "userGender":"male"
-```
-
-***
-
-#### - 로그인  
-
-- method : **POST**  
-- URL : **/sign_in**  
-
-```bash
-curl -v -X POST "http://localhost:4200/api/v1/auth/sign-in" \
- -d "userId=service123" \
- -d "userPassword=P!ssw0rd"
-```
-
-***
-
-#### - 아이디 찾기
-
-- method : **POST**  
-- URL : **/id_found**  
-
-```bash
-curl -v -X POST "http://localhost:4200/api/v1/auth/id_found" \
- -d "userEmail=email@email.com" \
- -d "authNumber=0123"
-```
-
-***
-
-#### - 아이디 찾기 확인
-
-- method : **GET**  
-- URL : **/id_found_value**
-
-```bash
-curl -v -X POST "http://localhost:4200/api/v1/auth/id_found_value" \
- -d "userId=service123" \
-```
-
-***
-
-#### - 비밀번호 찾기
-
-- method : **POST**  
-- URL : **/password_found**  
-
-```bash
-curl -v -X POST "http://localhost:4200/api/v1/auth/password_found" \
- -d "userId=service123" \
- -d "userEmail=email@email.com" \
- -d "authNumber=0123"
-```
-
-***
-
-#### - 비밀번호 찾기 재설정
-
-- method : **POST**  
-- URL : **/password_change**  
-
-```bash
-curl -v -X POST "http://localhost:4200/api/v1/auth/password_change" \
- -d '{"userPassword":"qwe123"}' \
-
-```
-
-***
-
-#### - 로그아웃
-
-- method : **POST**  
-- URL : **/logout**  
-
-```bash
-curl -v -X POST "http://localhost:4200/api/v1/auth/logout" \
- -H "Authorization: Bearer {JWT}"
-```
+export const SIGN_IN_REQUEST_URL = `${SERVER_AUTH_MODULE_URL}/sign-in`;
+export const EMAIL_AUTH_REQUEST_URL = `${SERVER_AUTH_MODULE_URL}/email-auth`;
+export const EMAIL_AUTH_CHECK_REQUEST_URL = `${SERVER_AUTH_MODULE_URL}/email-auth-check`;
+export const SIGN_UP_REQUEST_URL = `${SERVER_AUTH_MODULE_URL}/sign-up`;
 
 
-#### - 내 정보
+// description: 게시물 상수
 
-- method : **GET**  
-- URL : **/my_page**  
-
-```bash
-curl -v -X POST "http://localhost:4200/api/v1/auth/my_page" \
- -H "Authorization: Bearer {JWT}" \
-```
-
-***
-
-#### - 개인정보수정
-
-- method : **POST**  
-- URL : **/update**  
-
-```bash
-curl -v -X POST "http://localhost:4200/api/v1/auth/update" \
- -H "Authorization: Bearer {JWT}" \
- -d "userId=service123" \
- -d "userPassword=Pa55w0rd" \
- -d "userEmail=email@email.com" \
- -d "authNumber=0123" \
- -d "userAge":"20" \
- -d "userGender":"male"
-```
-
-***
-
-#### - 회원탈퇴
-
-- method : **POST**  
-- URL : **/user_delete**  
-
-```bash
-curl -v -X POST "http://localhost:4200/api/v1/auth/user_delete" \
- -H "Authorization: Bearer {JWT}" \
-```
-
-***
