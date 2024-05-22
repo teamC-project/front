62c96b5853d56f55d309708eb3f3ad315eed8dbb
ahndb <ahndb.dev@gmail.com>
2024-05-22 16:51:59 +0900

    1

--- src/App.tsx
+++ src/App.tsx
@@ -6,34 +6,36 @@ import { Route, Routes, useNavigate } from "react-router";
 import { AUTH_PATH, MAIN_OFF_PATH, MAIN_ON_PATH, SERVICE_PATH } from "./constant";
 import ServiceContainer from "./layouts/ServiceContainer";
 import Authentication from "./views/Authentication";
+import Main from "./views/Main";
 
-// component: root 경로 컴포넌트 //
-function Index() {
+// // component: root 경로 컴포넌트 //
+// function Index() {
 
-  //                    state                    //
-  const [cookies] = useCookies();
+//   //                    state                    //
+//   const [cookies] = useCookies();
 
-  //                    function                    //
-  const navigator = useNavigate();
+//   //                    function                    //
+//   const navigator = useNavigate();
 
-  //                    effect                    //
-  useEffect(() => {
-    const accessToken = cookies.accessToken;
-    if (accessToken) navigator(MAIN_ON_PATH);
-    else navigator(MAIN_OFF_PATH);
-  }, []);
+//   //                    effect                    //
+//   useEffect(() => {
+//     const accessToken = cookies.accessToken;
+//     if (accessToken) navigator(MAIN_ON_PATH);
+//     else navigator(MAIN_OFF_PATH);
+//   }, []);
 
-  //                    render                    //
-  return <></>;
-}
+//   //                    render                    //
+//   return <></>;
+// }
 
 function App() {
   return (
     <Routes >
-      <Route index element={<Index />} />
+      <Route path={MAIN_ON_PATH} element={<Main />} />
+      {/* <Route index element={<Index />} /> */}
       <Route path={AUTH_PATH} element={<Authentication />} />
       <Route path={SERVICE_PATH} element={<ServiceContainer />} >
-      {/* <Route path={SNS_PATH} element={<Sns />} /> */}
+        
       {/* <Route path={AUTH_PATH} element={<Authentication />} /> */}
       {/* <Route path={AUTH_PATH} element={<Authentication />} /> */}
       {/*  */}
