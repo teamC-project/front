3c3684c4394783a6584d9e152ff390494ff25631
heesanggod <gmltkd263@naver.com>
2024-05-23 16:57:19 +0900

    1

--- src/views/Service/Designer/DesignerWrite/index.tsx
+++ src/views/Service/Designer/DesignerWrite/index.tsx
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import "./style.css";
 
 export default function DesignerWrite() {
+
+    //              state               //
+    const contentsRef = useRef<HTMLTextAreaElement | null>(null);
 
     //                    render                    //
     return (
         <div id='designer-write-wrapper'>
             <div className='designer-write-top'>
                 <div className='designer-write-title-box'>
+                    <div className='designer-write-title'>제목</div>
                     <input className='designer-write-title-input' placeholder='제목을 입력해주세요.'></input>
                 </div>
             </div>
             <div className='designer-write-contents-box'>
                 <textarea className='designer-write-contents-textarea' placeholder='내용을 입력해주세요.'></textarea>
             </div>
-            <div className='write-button'>올리기</div>
+            <div className='write-button'>
+                <div className='button'>올리기</div>
+            </div>
         </div>
     );
 }
