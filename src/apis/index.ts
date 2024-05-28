6b72a9078a5a9811922bea62bdc93910f538710b
ahndb <ahndb.dev@gmail.com>
2024-05-28 09:03:17 +0900

    24.05.28 09:01 Delete Deduplication in apis and Update css about MyPage

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,5 +1,4 @@
 import { AxiosResponse } from "axios";
-<<<<<<< HEAD
 import ResponseDto from "./reponse.dto";
 
 
@@ -19,23 +18,3 @@ export const requestErrorHandler = (error: any) => {
 
 // function: Authorization Bearer 헤더
 export const bearerAuthorization = (accessToken:string) => ({ headers: { 'Authorization': `Bearer ${accessToken}` } })
-=======
-import ResponseDto from "./response.dto";
-
-// function: Request 처리 함수
-export const requestHandler = <T>(response: AxiosResponse<T, any>) => {
-  const responseBody = response.data;
-  return responseBody;
-};
-
-// function: Request Error 처리 함수
-export const requestErrorHandler = (error: any) => {
-  const responseBody = error.response?.data;
-  if (!responseBody) return null;
-  return responseBody as ResponseDto;
-};
-
-// function: Authorization Bearer 헤더
-export const bearerAuthorization = (accessToken: string) => ({ headers: { 'Authorization': `Bearer ${accessToken}`}})
-
->>>>>>> a30791c0894ceabc5a34115eb47dde213107301d
