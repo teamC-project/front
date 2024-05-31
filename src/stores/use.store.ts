5fc82f869b6944965fd12e3bbf59f76215a44064
kia0919 <john0256@naver.com>
2024-05-31 15:58:24 +0900

    개인정보수정 화면 작업

--- src/stores/use.store.ts
+++ src/stores/use.store.ts
@@ -1,18 +1,22 @@
 import { create } from "zustand";
 
 interface UserStore {
+  // accessToken: string;
+  // setAccessToken: (accessToken: string) => void;
   loginUserId: string;
   setLoginUserId: (loginUserId: string) => void;
   loginUserRole: string;
-  setloginUserRole: (loginUserRole: string) => void;
+  setLoginUserRole: (loginUserRole: string) => void;
 }
 
 export const useUserStore = create<UserStore>((set) => ({
+  // accessToken: "",
+  // setAccessToken: (accessToken: string) => set((state) => ({...state, accessToken})),
   loginUserId: "",
   setLoginUserId: (loginUserId: string) =>
     set((state) => ({ ...state, loginUserId })),
   loginUserRole: "",
-  setloginUserRole: (loginUserRole: string) =>
+  setLoginUserRole: (loginUserRole: string) =>
     set((state) => ({ ...state, loginUserRole })),
 }));
 
