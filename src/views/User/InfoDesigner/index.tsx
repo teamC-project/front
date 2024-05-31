0514aade48f56b2d00d318614e1b629cae02756c
kia0919 <john0256@naver.com>
2024-05-31 09:07:29 +0900

    designer css

--- src/views/User/InfoDesigner/index.tsx
+++ src/views/User/InfoDesigner/index.tsx
@@ -16,8 +16,8 @@ export default function InfoDesigner() {
   const [companyNameMessage, setCompanyNameMessage] = useState<string>('');
 
   const [isGenderCheck, setIsGenderCheck] = useState<boolean>(false);
-  const [isImageCheck,setIsImageCheck] = useState<boolean>(false);
-  const [isCompanyNameCheck,setIsCompanyNameCheck] = useState<boolean>(false);
+  const [isImageCheck, setIsImageCheck] = useState<boolean>(false);
+  const [isCompanyNameCheck, setIsCompanyNameCheck] = useState<boolean>(false);
   const [isCompanyNameError, setIsCompanyNameError] = useState<boolean>(false);
   const [isImageError, setIsImageError] = useState<boolean>(false);
 
@@ -40,14 +40,14 @@ export default function InfoDesigner() {
   };
 
   const onCompanyNameChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    const {value} = event.target;
+    const { value } = event.target;
     setCompanyName(value);
     setIsCompanyNameCheck(false);
     setCompanyNameMessage('');
   };
 
   const onImageChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    const {value} = event.target;
+    const { value } = event.target;
     setImage(value);
     setIsImageCheck(false);
     setImageMessage('');
@@ -61,32 +61,30 @@ export default function InfoDesigner() {
         <div className='white-space2'></div>
 
         <div className='info-designer-container'>
-          <div className='designer-id-contents'>아이디</div>
-          <div className='designer-id-container'>
-            <div className='id-input-box'>
-              <div className='designer-id-info'>asd</div>
+          <div className='designer-id-contents'>
+            <div className='designer-id'>아이디</div>
+            <div className='designer-id-container'>
+              <div className='id-input-box'>
+                <div className='designer-id-info'>asd</div>
+              </div>
             </div>
           </div>
-        </div>
-
-        <div className='info-designer-box-text'>
-          <div className='info-designer-text'>성별</div>
-          <div className='info-designer-next-box'>
-            <div className='info-designer-radio-box'>
-              <InputBox label={'MALE'} type={'radio'} value={gender} name={'gender'} message={genderMessage} onChangeHandler={onGenderChangeHandler} />
-            </div>
-            <div className='info-designer-radio-box'>
-              <InputBox label={'FEMALE'} type={'radio'} value={gender} name={'gender'} message={genderMessage} onChangeHandler={onGenderChangeHandler} />
+          <div className='info-designer-box-text'>
+            <div className='info-designer-text'>성별</div>
+            <div className='info-designer-next-box'>
+              <div className='info-designer-radio-box'>
+                <InputBox label={'MALE'} type={'radio'} value={gender} name={'gender'} message={genderMessage} onChangeHandler={onGenderChangeHandler} />
+              </div>
+              <div className='info-designer-radio-box'>
+                <InputBox label={'FEMALE'} type={'radio'} value={gender} name={'gender'} message={genderMessage} onChangeHandler={onGenderChangeHandler} />
+              </div>
             </div>
           </div>
-        </div>
-
-        <div className='info-designer-box-text'>
-          <div className='info-designer-text'>연령대</div>
-          <SelectBox value={age} onChange={onAgeChangeHandler} />
-        </div>
-
-        <div className='info-designer-update-box-text'>
+          <div className='info-designer-box-text'>
+            <div className='info-designer-text'>연령대</div>
+            <SelectBox value={age} onChange={onAgeChangeHandler} />
+          </div>
+          <div className='info-designer-update-box-text'>
             <div className='info-designer-update-text'>업체명</div>
             <div className='info-designer-update-next-box'><InputBox type={'text'} value={companyName} placeholder={'업체명을 입력해주세요.'} onChangeHandler={onCompanyNameChangeHandler} message={companyNameMessage} error={isCompanyNameError} /></div>
           </div>
@@ -95,6 +93,10 @@ export default function InfoDesigner() {
             <div className='info-designer-update-text'>면허증사진</div>
             <div className='info-designer-update-next-box'><InputBox type={'file'} value={image} placeholder={''} onChangeHandler={onImageChangeHandler} message={imageMessage} error={isImageError} /></div>
           </div>
+          <div className='submit-box'>
+            <div className='complete-text primary-button'>완료</div>
+          </div>
+        </div>
 
         <div className='white-space2'></div>
       </div>
