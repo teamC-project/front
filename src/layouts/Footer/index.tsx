c33154c9b0c17345e57aa7f6f17e55c88d78ea4e
ahndb <ahndb.dev@gmail.com>
2024-06-05 17:54:12 +0900

    BackUp frontend

--- src/layouts/Footer/index.tsx
+++ src/layouts/Footer/index.tsx
@@ -3,7 +3,28 @@ import "./style.css";
 
 export default function Footer() {
   return (
-    <div>index</div>
-    
+    <div className='footer-size-check'>
+      <div className=''>
+        <div className=''>사이트소개</div>
+        <div className=''>|</div>
+        <div className=''>이용약관</div>
+        <div className=''>|</div>
+        <div className=''>개인정보처리방침</div>
+        <div className=''>|</div>
+        <div className=''>청소년보호정책</div>
+        <div className=''>|</div>
+        <div className=''>광고/제휴</div>
+        <div className=''>|</div>
+        <div className=''>메일문의</div>
+      </div>
+      <div className=''>
+        <div className=''>헤어어드바이저 2024~ <span className=''>&copy;</span> All Rights Reserved.</div>
+        <div className=''>
+          <div className=''>오늘 접속자</div>
+          <div>|</div>
+          <div className=''>총 접속자</div>
+        </div>
+      </div>
+    </div>
   )
 }
