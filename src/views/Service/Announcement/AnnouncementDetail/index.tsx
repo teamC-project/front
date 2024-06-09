a8247252ee6228e60869222c918ffd9cee910386
GODDONGHYUN <leenl1168@gmail.com>
2024-06-10 03:15:08 +0900

    2024-06-10 공지사항, QNA 프론트 작업 미완성 PULL 받지 말것

--- src/views/Service/Announcement/AnnouncementDetail/index.tsx
+++ src/views/Service/Announcement/AnnouncementDetail/index.tsx
@@ -1,9 +1,90 @@
-import React from 'react'
+import React, { ChangeEvent, useState } from 'react';
+import './style.css';
+import { useNavigate, useParams } from 'react-router';
 
-export default function index() {
-	return (
-		<div>
-			
-		</div>
-	)
+
+import { GetAnnouncementBoardListResponseDto, GetAnnouncementBoardResponseDto } from 'src/apis/announcement/dto/response';
+import ResponseDto from 'src/apis/response.dto';
+import { DESIGNER_BOARD_LIST_ABSOLUTE_PATH, DESIGNER_BOARD_UPDATE_ABSOLUTE_PATH, MAIN_PATH } from 'src/constant';
+import { useCookies } from 'react-cookie';
+import { useUserStore } from 'src/stores';
+
+
+//                    component                    //
+export default function AnnouncementDetail() {
+
+    //              state               //
+    const { loginUserId, loginUserRole } = useUserStore();
+    const { announcementBoardNumber } = useParams();
+    const { announcementBoardCommentNumber = '' } = useParams<{ announcementBoardCommentNumber: string }>();
+    const [cookies] = useCookies();
+
+    const [title, setTitle] = useState<string>('');
+    const [writerId, setWriterId] = useState<string>('');
+    const [writeDate, setWriteDate] = useState<string>('');
+    const [viewCount, setViewCount] = useState<number>(0);
+    const [contents, setContents] = useState<string>('');
+    const [comment, setComment] = useState<string | null>(null);
+
+    //                  function                    //
+    const navigate = useNavigate();
+
+    const getAnnouncementBoardResponse = (result: GetAnnouncementBoardResponseDto | ResponseDto | null) => {
+        const message = 
+        !result ? '서버에 문제가 있습니다.' :
+        result.code === 'VF' ? '잘못된 접수 번호입니다.' :
+        result.code === 'AF' ? '인증에 실패 했습니다.' :
+        result.code === 'NB' ? '존재하지 않는 게시물 입니다.' :
+        result.code === 'DBE' ? '서버에 문제가 있습니다.' : '';
+
+        if (!result || result.code !== 'SU') {
+            alert(message);
+            if (result?.code === 'AF') {
+                navigate(MAIN_PATH);
+                return;
+            }
+            navigate(DESIGNER_BOARD_LIST_ABSOLUTE_PATH);
+            return;
+        }
+
+        const { title, writerId, writeDatetime, viewCount, contents, comment } = result as GetAnnouncementBoardResponseDto;
+        setTitle(title);
+        setWriterId(writerId);
+        setWriteDate(writeDatetime);
+        setViewCount(viewCount);
+        setContents(contents);
+        setComment(comment);
+    };
+
+    //                   event handler                    //
+    const handleGoToList = () => {
+        navigate(DESIGNER_BOARD_LIST_ABSOLUTE_PATH);
+    };
+    
+    const onUpdateClickHandler = () => {
+        if (!announcementBoardNumber || loginUserId !== writerId) return;
+        navigate(DESIGNER_BOARD_UPDATE_ABSOLUTE_PATH(announcementBoardNumber));
+    };
+
+    //              render              //
+    return (
+    <div className="announcement-detail">
+        <div className="announcement-detail-title">제목{title}</div>
+        <div className="announcement-detail-container">
+            <div className="announcement-detail-information">
+                <div className="announcement-detail-information1">작성자{writerId}</div>
+                <div className="announcement-detail-information2">작성일{writeDate}</div>
+                <div className="announcement-detail-information3">조회{viewCount}</div>
+                <div className="announcement-detail-information4">삭제</div>
+                <div className="announcement-detail-information5" onClick={onUpdateClickHandler}>수정</div>
+            </div>
+        </div>
+        <div className="announcement-detail-view">
+        </div>
+
+        <div className="announcement-detail-go-to-announcementList" onClick={handleGoToList}>
+        목록으로
+        </div>
+    </div>
+    );
 }
--- src/views/Service/Qna/QnaDetail/index.tsx
+++ src/views/Service/Qna/QnaDetail/index.tsx
@@ -1,35 +1,206 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './style.css';
 import'../../../../App.css'
 import { useNavigate, useParams } from 'react-router';
-import {  QNA_BOARD_LIST_ABSOLUTE_PATH } from 'src/constant';
+import {  AUTH_ABSOLUTE_PATH, QNA_BOARD_LIST_ABSOLUTE_PATH, QNA_BOARD_UPDATE_ABSOLUTE_PATH } from 'src/constant';
 import { useUserStore } from 'src/stores';
 import { useCookies } from 'react-cookie';
 import ResponseDto from 'src/apis/response.dto';
+import { PostQnaBoardCommentRequestDto } from 'src/apis/QnaBoard/dto/request';
+import { getQnaBoardRequest } from 'src/apis/QnaBoard';
+import { GetQnaBoardResponseDto } from 'src/apis/QnaBoard/dto/response';
 
 //												component													//
 export default function QnaDetail() {
 
 	//										state										//
-	const {loginUserId, loginUserRole} = useUserStore();
-	const {qnaBoardNumber} = useParams();
-	const [cookies] = useCookies();
+const { loginUserId, loginUserRole } = useUserStore();
+  const { receptionNumber } = useParams();
 
-	const [title, setTItle] = useState<string>('');
-	const [writerId, setWriterId] = useState<string>('');
-	const [writeDate, setWriteDate] = useState<string>('');
-	const [viewCount, setViewCount] = useState<number>(0);
-	const [contents, setContents] = useState<string>('');
-	const [status, setStatus] = useState<string | null>(null);
-	const [comment, setComment] = useState<string | null>(null);
-	const [commentRows, setCommentRows] = useState<number>(1);
+  const [cookies] = useCookies();
+  const [title, setTitle] = useState<string>("");
+  const [writerId, setWriterId] = useState<string>("");
+  const [writeDatetime, setWriteDatetime] = useState<string>("");
+  const [viewCount, setViewCount] = useState<number>(0);
+  const [contents, setContents] = useState<string>("");
+  const [status, setStatus] = useState<boolean>(false);
+  const [comment, setComment] = useState<string | null>(null);
+  const [commentRows, setCommentRows] = useState<number>(1);
 
-	  //                    function                    //
-		const navigator = useNavigate();
+  //                    function                    //
+  const navigator = useNavigate();
 
-		const increaseViewCountResponse = (result : ResponseDto | null) => {
-			
-		}
+  const increaseViewCountResponse = (result: ResponseDto | null) => {
+    const message = !result
+      ? "서버에 문제가 있습니다."
+      : result.code === "VF"
+      ? "잘못된 접수번호입니다."
+      : result.code === "AF"
+      ? "인증에 실패했습니다."
+      : result.code === "NB"
+      ? "존재하지 않는 접수번호입니다."
+      : result.code === "DBE"
+      ? "서버에 문제가 있습니다."
+      : "";
+
+    if (!result || result.code !== "SU") {
+      alert(message);
+      if (result?.code === "AF") {
+        navigator(AUTH_ABSOLUTE_PATH);
+        return;
+      }
+      navigator(QNA_BOARD_LIST_ABSOLUTE_PATH);
+      return;
+    }
+
+    if (!cookies.accessToken || !receptionNumber) return;
+    getQnaBoardRequest(receptionNumber, cookies.accessToken).then(
+      getBoardResponse
+    );
+  };
+
+  const getBoardResponse = (
+    result: GetQnaBoardResponseDto | ResponseDto | null
+  ) => {
+    const message = !result
+      ? "서버에 문제가 있습니다."
+      : result.code === "VF"
+      ? "잘못된 접수번호입니다."
+      : result.code === "AF"
+      ? "인증에 실패했습니다."
+      : result.code === "NB"
+      ? "존재하지 않는 접수번호입니다."
+      : result.code === "DBE"
+      ? "서버에 문제가 있습니다."
+      : "";
+
+    if (!result || result.code !== "SU") {
+      alert(message);
+      if (result?.code === "AF") {
+        navigator(AUTH_ABSOLUTE_PATH);
+        return;
+      }
+      navigator(QNA_BOARD_LIST_ABSOLUTE_PATH);
+      return;
+    }
+
+    const {
+      qnaBoardTitle,
+      qnaBoardWriterId,
+      qnaBoardWriteDatetime,
+      qnaBoardViewCount,
+      qnaBoardContents,
+      qnaBoardStatus,
+      qnaBoardComment,
+    } = result as GetQnaBoardResponseDto;
+    setTitle(title);
+    setWriterId(writerId);
+    setWriteDatetime(writeDatetime);
+    setViewCount(viewCount);
+    setContents(contents);
+    setStatus(status);
+    setComment(comment);
+  };
+
+  const postCommentResponse = (result: ResponseDto | null) => {
+    const message = !result
+      ? "서버에 문제가 있습니다."
+      : result.code === "AF"
+      ? "권한이 없습니다."
+      : result.code === "VF"
+      ? "입력 데이터가 올바르지 않습니다."
+      : result.code === "NB"
+      ? "존재하지 않는 게시물입니다."
+      : result.code === "WC"
+      ? "이미 답글이 작성된 게시물입니다."
+      : result.code === "DBE"
+      ? "서버에 문제가 있습니다."
+      : "";
+
+    if (!result || result.code !== "SU") {
+      alert(message);
+      return;
+    }
+
+    if (!receptionNumber || !cookies.accessToken) return;
+    getQnaBoardRequest(receptionNumber, cookies.accessToken).then(
+      getBoardResponse
+    );
+  };
+
+  const deleteBoardResponse = (result: ResponseDto | null) => {
+    const message = !result
+      ? "서버에 문제가 있습니다."
+      : result.code === "AF"
+      ? "권한이 없습니다."
+      : result.code === "VF"
+      ? "올바르지 않은 접수 번호입니다."
+      : result.code === "NB"
+      ? "존재하지 않는 접수 번호입니다."
+      : result.code === "DBE"
+      ? "서버에 문제가 있습니다."
+      : "";
+
+    if (!result || result.code !== "SU") {
+      alert(message);
+      return;
+    }
+
+    navigator(QNA_BOARD_LIST_ABSOLUTE_PATH);
+  };
+
+  //                    event handler                    //
+  const onCommentChangeHandler = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    if (status || loginUserRole !== "ROLE_ADMIN") return;
+    const comment = event.target.value;
+    setComment(comment);
+
+    const commentRows = comment.split("\n").length;
+    setCommentRows(commentRows);
+  };
+
+  const onCommentSubmitClickHandler = () => {
+    if (!comment || !comment.trim()) return;
+    if (
+      !receptionNumber ||
+      loginUserRole !== "ROLE_ADMIN" ||
+      !cookies.accessToken
+    )
+      return;
+
+    const requestBody: PostQnaBoardCommentRequestDto = { comment };
+    postCommentRequest(receptionNumber, requestBody, cookies.accessToken).then(
+      postCommentResponse
+    );
+  };
+
+  const onListClickHandler = () => {
+    navigator(QNA_BOARD_LIST_ABSOLUTE_PATH);
+  };
+
+  const onUpdateClickHandler = () => {
+    if (!receptionNumber || loginUserId !== writerId || status) return;
+    navigator(QNA_BOARD_UPDATE_ABSOLUTE_PATH(receptionNumber));
+  };
+
+  const onDeleteClickHandler = () => {
+    if (!receptionNumber || loginUserId !== writerId || !cookies.accessToken)
+      return;
+    const isConfirm = window.confirm("정말로 삭제하시겠습니까?");
+    if (!isConfirm) return;
+
+    deleteQnaBoardRequest(receptionNumber, cookies.accessToken).then(
+      deleteBoardResponse
+    );
+  };
+
+  //                    effect                    //
+  useEffect(() => {
+    if (!cookies.accessToken || !receptionNumber) return;
+    increaseQnaBoardCountRequest(receptionNumber, cookies.accessToken).then(
+      increaseViewCountResponse
+    );
+  }, []);
 	return (
     <div id="qna-board-detail-wrapper">
       <div className="qna-board-detail-title">제목</div>
@@ -69,3 +240,11 @@ export default function QnaDetail() {
     </div>
   );
 }
+function deleteQnaBoardRequest(receptionNumber: string, accessToken: any) {
+  throw new Error('Function not implemented.');
+}
+
+function increaseQnaBoardCountRequest(receptionNumber: string, accessToken: any) {
+  throw new Error('Function not implemented.');
+}
+
