a5e5f60e35ab6671dbc8d4402211986337f91b41
heesanggod <gmltkd263@naver.com>
2024-06-21 15:28:28 +0900

    123

--- src/views/Service/Chat/index.tsx
+++ src/views/Service/Chat/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { KeyboardEvent, useEffect, useRef, useState } from 'react'
 import './style.css';
 import { GetChatMessageListResponseDto, GetChatMessageResponseDto, GetChatroomResponseDto } from 'src/apis/chat/dto/response';
 import ResponseDto from 'src/apis/response.dto';
@@ -26,7 +26,7 @@ export default function ChatroomDetail() {
   const [senderId, setSenderId] = useState<string>('');
   const [sendDatetime, setSendDatetime] = useState<string>('');
   const [message, setMessage] = useState<string>('');
-
+  const [opponentId, setOpponentId] = useState<string>('');
 
   //                    function                    //
   const getChatroomResponse = (result: GetChatroomResponseDto | ResponseDto | null) => {
@@ -48,6 +48,7 @@ export default function ChatroomDetail() {
     } = result as GetChatroomResponseDto;
 
     setRoomName(roomName);
+    setOpponentId(loginUserId === customerId ? designerId : customerId);
   };
   
   const getChatMessagesResponse = (result: GetChatMessageResponseDto | ResponseDto | null) => {
@@ -66,7 +67,7 @@ export default function ChatroomDetail() {
 
     const { chatMessageList } = result as GetChatMessageListResponseDto;
     setMessages(chatMessageList);
-
+    
   };
 
   const deleteChatroomResponse = (result: ResponseDto | null) => {
@@ -108,6 +109,11 @@ export default function ChatroomDetail() {
         .then(deleteChatroomResponse)
 };
 
+const onPasswordKeydownHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+  if (event.key !== 'Enter') return;
+  onMessageSendHandler();
+};
+
   //                    effect                    //
   useEffect(() => {
     if (flag) return;
@@ -130,29 +136,34 @@ export default function ChatroomDetail() {
     if (!cookies.accessToken || !roomId) return;
     getChatMessagesRequest(roomId, cookies.accessToken)
       .then(getChatMessagesResponse);
+    getChatroomRequest(roomId, cookies.accessToken)
+      .then(getChatroomResponse);
   }, [cookies.accessToken, roomId]);
-
-  // useEffect(() => {
-  //   if (!roomId) return;
-  //   deleteChatroomRequest(roomId, cookies.accessToken)
-  // }, [])
 
   //                    render                       //
   return (
-    <div className="chat-room-container">
-      <div className="chat-room-header">
-        <h2>{roomname}</h2>
-        <button className="delete-button" onClick={onDeleteButtonClickHandler}>
-          <svg className="delete-svgIcon" viewBox="0 0 448 512">
-            <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"></path>
-          </svg>
-        </button>
-        <button className='out-button' onClick={onChatExitClickHandler}>
-          <div className="sign"><svg viewBox="0 0 512 512"><path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"></path></svg></div>
-          <div className="text">나가기</div>
-        </button>
+    <div className='chat-room-container'>
+      <div className='chat-room-header'>
+        <div>{roomname}</div>
       </div>
-      <div className="cha-troom-messages">
+      <div className='chat-room-subheader'>
+        <div className='opponent-id'>{opponentId}</div>
+        <div className='button-wrap'>
+          <button className='delete-button' onClick={onDeleteButtonClickHandler}>
+            <svg className="delete-svgIcon" viewBox="0 0 448 512">
+              <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"></path>
+            </svg>
+          </button>
+          <button className='out-button' onClick={onChatExitClickHandler}>
+            <div className="sign"><svg viewBox="0 0 512 512"><path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"></path></svg></div>
+            <div className='text'>나가기</div>
+          </button>
+        </div>
+      </div>
+        
+      
+      
+      <div className="chat-room-messages">
         {messages.map((item, index) => (
           <div key={index} className={item.senderId === loginUserId ? 'message-sent' : 'message-received'}>
             <div className="message-content">{item.message}</div>
@@ -165,6 +176,7 @@ export default function ChatroomDetail() {
           value={newMessage} 
           onChange={(e) => setNewMessage(e.target.value)} 
           placeholder="메시지를 입력하세요" 
+          onKeyDown={onPasswordKeydownHandler}
         />
         <button className='send-button' onClick={onMessageSendHandler}>전송</button>
       </div>
