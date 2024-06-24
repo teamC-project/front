5629712fea4f63ba2ced017a7d2a04da86f9da04
GODDONGHYUN <leenl1168@gmail.com>
2024-06-24 17:42:29 +0900

    2024-06-24 트렌드, 공지사항 게시판 css 수정

--- src/views/Service/Trend/TrendList/index.tsx
+++ src/views/Service/Trend/TrendList/index.tsx
@@ -163,23 +163,20 @@ export default function TrendList() {
     getTrendBoardListRequest(cookies.accessToken).then(getTrendBoardResponse);
   }, []);
 
-  const searchButtonClass = searchWord ? 'primary-button' : 'disable-button';
 
   return (
     <div id='trend-board-wrapper'>
-      <div className="trend-board-list-top-bar">
-        <div className='trend-board-search-box'>
-          <div className='trend-board-search-keyword'>검색 키워드</div>
+        <div className='trend-board-list-search-box'>
+          <div className='trend-board-list-search-keyword'>검색 키워드</div>
           <input 
-            className='trend-board-search-input' 
+            className='trend-board-list-search-input' 
             placeholder='제목을 입력하세요.'
             value={searchWord} 
             onChange={onSearchWordChangeHandler}
             onKeyDown={onSearchInputKeyDown}
           />
-          <div className={searchButtonClass} onClick={onSearchButtonClickHandler}>검색</div>
+          <div className='trend-board-list-search-input-button' onClick={onSearchButtonClickHandler}>검색</div>
         </div>
-      </div>
       <div className="trend-board-list-container">
         <div className='trend-board-list'>
           {viewList.map(item => <CardItem key={item.trendBoardNumber} {...item} />)}
