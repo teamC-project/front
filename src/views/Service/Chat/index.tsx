126c20d64c844d20d31883b6259f9f65d6abf568
heesanggod <gmltkd263@naver.com>
2024-06-28 10:14:09 +0900

    designer

--- src/views/Service/Chat/index.tsx
+++ src/views/Service/Chat/index.tsx
@@ -33,17 +33,19 @@ import './style.css';
 export default function ChatroomDetail() {
 
 //                          state                           //
-    const navigator = useNavigate();
-    const [cookies] = useCookies();
-    const { roomId, resetRoomId } = useChatStore();
     const { loginUserId } = useUserStore();
-    const [roomname, setRoomName] = useState<string>('');
-    const [messages, setMessages] = useState<ChatMessageList[]>([]);
-    const [newMessage, setNewMessage] = useState<string>('');
+    const { roomId, resetRoomId } = useChatStore();
 
+    const [cookies] = useCookies();
+
+    const [roomname, setRoomName] = useState<string>('');
     const [opponentId, setOpponentId] = useState<string>('');
+    const [newMessage, setNewMessage] = useState<string>('');
+    const [messages, setMessages] = useState<ChatMessageList[]>([]);
 
 //                          function                            //
+    const navigator = useNavigate();
+
     const getChatroomResponse = (result: GetChatroomResponseDto | ResponseDto | null) => {
         const message = 
             !result ? '서버에 문제가 있습니다.' :
@@ -173,7 +175,7 @@ export default function ChatroomDetail() {
                     <div className="sign"><svg viewBox="0 0 512 512"><path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"></path></svg></div>
                     <div className='text'>나가기</div>
                 </button>
-                </div>
+            </div>
         </div>
         <div className="chat-room-messages">
             {messages.map((item, index) => (
@@ -181,17 +183,17 @@ export default function ChatroomDetail() {
                 <div className="message-content">{item.message}</div>
             </div>
             ))}
-            </div>
-            <div className="chat-room-footer">
-                <input 
-                    type="text" 
-                    value={newMessage} 
-                    onChange={(e) => setNewMessage(e.target.value)} 
-                    placeholder="메시지를 입력하세요" 
-                    onKeyDown={onPasswordKeydownHandler}
-                />
-                <button className='send-button' onClick={onMessageSendHandler}>전송</button>
-                </div>
+        </div>
+        <div className="chat-room-footer">
+            <input 
+                type="text" 
+                value={newMessage} 
+                onChange={(e) => setNewMessage(e.target.value)} 
+                placeholder="메시지를 입력하세요" 
+                onKeyDown={onPasswordKeydownHandler}
+            />
+            <button className='send-button' onClick={onMessageSendHandler}>전송</button>
+        </div>
     </div>
     );
 }
--- src/views/Service/Designer/DesignerList/index.tsx
+++ src/views/Service/Designer/DesignerList/index.tsx
@@ -81,15 +81,15 @@ export default function DesignerList() {
 	const navigator = useNavigate();
 
 	const getDesignerBoardResponse = (result: GetDesignerBoardListResponseDto | ResponseDto | null) => {
-    const message =
+	const message =
 		!result ? '서버에 문제가 있습니다.' :
 		result.code === 'AF' ? '인증에 실패했습니다.' :
 		result.code === 'DBE' ? '서버에 문제가 있습니다.' : '';
 
 		if (!result || result.code !== 'SU') {
-        alert(message);
-        if (result?.code === 'AF') navigator(MAIN_PATH);
-        return;
+		alert(message);
+		if (result?.code === 'AF') navigator(MAIN_PATH);
+		return;
 		}
 
 		const { designerBoardList } = result as GetDesignerBoardListResponseDto;
@@ -105,40 +105,40 @@ export default function DesignerList() {
 			result.code === 'VF' ? '검색어를 입력하세요.' :
 			result.code === 'AF' ? '인증에 실패했습니다.' :
 			result.code === 'DBE' ? '서버에 문제가 있습니다.' : '';
-	
-		if (!result || result.code !== 'SU') {
-		alert(message);
-		if (result?.code === 'AF') navigator(MAIN_PATH);
-		return;
-    }
 
-    const { designerBoardList } = result as GetSearchDesignerBoardListResponseDto;
-    const updatedDesignerBoardList = designerBoardList.map(item => ({
-		...item,
-    }));
-    setBoardList(updatedDesignerBoardList);
-    changeBoardList(updatedDesignerBoardList);
-    changePage(updatedDesignerBoardList, updatedDesignerBoardList.length);
-    setCurrentPage(!updatedDesignerBoardList.length ? 0 : 1);
-    setCurrentSection(!updatedDesignerBoardList.length ? 0 : 1);
+		if (!result || result.code !== 'SU') {
+			alert(message);
+			if (result?.code === 'AF') navigator(MAIN_PATH);
+			return;
+		}
+
+		const { designerBoardList } = result as GetSearchDesignerBoardListResponseDto;
+		const updatedDesignerBoardList = designerBoardList.map(item => ({
+			...item,
+		}));
+		setBoardList(updatedDesignerBoardList);
+		changeBoardList(updatedDesignerBoardList);
+		changePage(updatedDesignerBoardList, updatedDesignerBoardList.length);
+		setCurrentPage(!updatedDesignerBoardList.length ? 0 : 1);
+		setCurrentSection(!updatedDesignerBoardList.length ? 0 : 1);
 	};
 
 	const fetchDesignerBoardList = () => {
-    getSearchDesignerBoardListRequest('', cookies.accessToken).then(getDesignerBoardResponse);
+		getSearchDesignerBoardListRequest('', cookies.accessToken).then(getDesignerBoardResponse);
 	};
 
 //                          event handler                           //
 	const onWriteButtonClickHandler = () => {
-    navigator(DESIGNER_BOARD_WRITE_ABSOLUTE_PATH);
+		navigator(DESIGNER_BOARD_WRITE_ABSOLUTE_PATH);
 	};
 
 	const onSearchWordChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    const searchWord = event.target.value;
-    setSearchWord(searchWord);
-    if (!searchWord) {
-		setIsSearched(false);
-		fetchDesignerBoardList();
-    }
+		const searchWord = event.target.value;
+		setSearchWord(searchWord);
+		if (!searchWord) {
+			setIsSearched(false);
+			fetchDesignerBoardList();
+		}
 	};
 
 	const onSearchButtonClickHandler = (event: React.MouseEvent<HTMLDivElement>) => {
@@ -146,19 +146,19 @@ export default function DesignerList() {
 	};
 
 	const onSearchInputKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === 'Enter') {
-    handleSearch();
-    }
+		if (event.key === 'Enter') {
+			handleSearch();
+		}
 	};
-	
+
 	const handleSearch = () => {
 		if (!searchWord) {
 		alert('검색어를 입력하세요.');
 		return;
-    }
+		}
 	
 		if (!cookies.accessToken) return;
-	
+
 		setIsSearched(true);
 		getSearchDesignerBoardListRequest(searchWord, cookies.accessToken)
 		.then(getSearchDesignerBoardListResponse);
@@ -179,53 +179,48 @@ export default function DesignerList() {
 
 //                          render                          //
 return (
-    <div className='designerboard-list-wrapper'>
+	<div className='designerboard-list-wrapper'>
 		<div className='designerboard-list-search-box'>
-        <div className='designerboard-list-search-keyword'>검색 키워드</div>
-        <div className='designerboard-list-search-input-box'>
-        <input
-            className='designerboard-list-search-input'
-            placeholder='검색어를 입력하세요.'
-            value={searchWord}
-            onChange={onSearchWordChangeHandler}
-            onKeyDown={onSearchInputKeyDown}
-        />
-        </div>
-        <div className='designerboard-list-search-input-button' onClick={onSearchButtonClickHandler}>
-        검색
-        </div>
-    </div>
-    <div className='designerboard-list-table'>
-        <div className='designerboard-table-th'>
-			<div className='designerboard-list-table-board-number-top'>번호</div>
-			<div className='designerboard-list-table-title-top'>제목</div>
-			<div className='designerboard-list-table-writer-id-top'>작성자</div>
-			<div className='designerboard-list-table-write-date-top'>작성일</div>
-			<div className='designerboard-list-table-viewcount-top'>조회수</div>
-        </div>
-        {viewList.map(item => <ListItem {...item} />)}
+			<div className='designerboard-list-search-keyword'>검색 키워드</div>
+			<div className='designerboard-list-search-input-box'>
+				<input
+					className='designerboard-list-search-input'
+					placeholder='검색어를 입력하세요.'
+					value={searchWord}
+					onChange={onSearchWordChangeHandler}
+					onKeyDown={onSearchInputKeyDown}
+				/>
+			</div>
+			<div className='designerboard-list-search-input-button' onClick={onSearchButtonClickHandler}>검색</div>
+		</div>
+		<div className='designerboard-list-table'>
+			<div className='designerboard-table-th'>
+				<div className='designerboard-list-table-board-number-top'>번호</div>
+				<div className='designerboard-list-table-title-top'>제목</div>
+				<div className='designerboard-list-table-writer-id-top'>작성자</div>
+				<div className='designerboard-list-table-write-date-top'>작성일</div>
+				<div className='designerboard-list-table-viewcount-top'>조회수</div>
+			</div>
+			{viewList.map(item => <ListItem {...item} />)}
 		</div>
 		<div className='designerboard-list-bottom'>
-        <div className='designerboard-list-pagenation'>
-			<div className='page-left' onClick={onPreSectionClickHandler}></div>
-			<div className='designerboard-list-page-box'>
-            {pageList.map(page => 
-				page === currentPage ? 
-				<div className='designerboard-list-page-active'>{page}</div> :
-				<div className='designerboard-list-page' onClick={() => onPageClickHandler(page)}>{page}</div>
-            )}
+			<div className='designerboard-list-pagenation'>
+				<div className='page-left' onClick={onPreSectionClickHandler}></div>
+				<div className='designerboard-list-page-box'>
+					{pageList.map(page => 
+					page === currentPage ? 
+					<div className='designerboard-list-page-active'>{page}</div> :
+					<div className='designerboard-list-page' onClick={() => onPageClickHandler(page)}>{page}</div>
+					)}
+				</div>
+				<div className='page-right' onClick={onNextSectionClickHandler}></div>
 			</div>
-			<div className='page-right' onClick={onNextSectionClickHandler}></div>
-        </div>
-        <div className='designerboard-list-write-button-container'>
-        {loginUserRole === 'ROLE_DESIGNER' && (
-            <div className='designerboard-list-write-button' onClick={onWriteButtonClickHandler}>
-				글쓰기
-            </div>
+			<div className='designerboard-list-write-button-container'>
+			{loginUserRole === 'ROLE_DESIGNER' && (
+				<div className='designerboard-list-write-button' onClick={onWriteButtonClickHandler}>글쓰기</div>
 			)}
-        </div>
+			</div>
 		</div>
-    </div>
-);
-
+	</div>
+	);
 }
